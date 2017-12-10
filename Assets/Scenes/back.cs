64f71eaa8463bcf49c8708aa7e4c06feded99d8b
Gabriel Ramirez <g_e_ramirez@outlook.com>
2017-12-10 18:10:34 -0500

    for anything and anywhere

---
+++ Assets/Scenes/back.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Prime31.TransitionKit;
+
+public class back : MonoBehaviour {
+
+	public void cambiarNivel(){
+
+		TransitionKit.instance.transitionWithDelegate( fishEye );
+	}
+
+	FishEyeTransition fishEye = new FishEyeTransition()
+	{
+		nextScene = 0,
+		duration = 1.0f,
+		size = 0.08f,
+		zoom = 10.0f,
+		colorSeparation = 3.0f
+	};
+}
---
+++ Assets/creditos.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Prime31.TransitionKit;
+
+public class creditos : MonoBehaviour {
+
+	public void cambiarNivel(){
+
+		TransitionKit.instance.transitionWithDelegate( fishEye );
+	}
+
+	FishEyeTransition fishEye = new FishEyeTransition()
+	{
+		nextScene = 5,
+		duration = 1.0f,
+		size = 0.08f,
+		zoom = 10.0f,
+		colorSeparation = 3.0f
+	};
+}
